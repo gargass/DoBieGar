9bc072612909d48978835d14bde2760a427b04f1
Sebastian <sebastian.gargas92@gmail.com>
2016-02-09 23:09:15 +0100

    Tworzenie współistniejących genów
    poprawa geny_wspolne_nowotwor

--- geny_wspolne_nowotwor.R
+++ geny_wspolne_nowotwor.R
@@ -1,30 +1,36 @@
 #nowotwory= c("GBMLGG", "BRCA", "KIPAN", "COADREAD", "STES", "GBM", "OV",
 #             "UCEC", "KIRC", "HNSC", "LUAD", "LGG", "LUSC", "THCA")
-nowotwor= 'BRCA'
-dane = read.table(paste('C:/Users/Gosia/Documents/DoBieGar/Zbiory/', nowotwor,'.txt' ,sep=''), h=T)
+nowotwor <- 'BRCA'
+dane <- read.table(paste('Zbiory/', nowotwor,'.txt' ,sep=''), h=T)
 
-geny <- read.table('C:/Users/Gosia/Documents/DoBieGar/p_value/lista_interesujacych_genow.txt', h=T)
-wspolne = matrix(0, nrow = 535, ncol=535)
-geny$x
+geny <- read.table('p_value/lista_interesujacych_genow.txt', h=T)
+wspolne <- data.frame(matrix(0, nrow = nrow(geny), ncol=nrow(geny)))
+
 colnames(wspolne)<-geny$x
 rownames(wspolne)<-geny$x
 
 
-for (i in 1:length(geny))
+for (i in 1:nrow(geny))
 {
-  for (j in i:length(geny))
+  gen_i <- as.character(geny$x[i])
+  n.gen_i <- sum(as.numeric(dane[,gen_i] == 1))
+  for (j in i:nrow(geny))
   {
-    if (sum(as.numeric(dane[,i+1]))!=0)
-    {
-    wspolne[i, j] = sum(as.numeric(dane[dane[, i+1]==1 & dane[, j+1]==1,j+1]))/sum(as.numeric(dane[,i+1]))
-    wspolne[j, i]=wspolne[i, j]
+    gen_j <- as.character(geny$x[j])
+    
+    n.gen_j <- sum(as.numeric(dane[,gen_j] == 1 & dane[, gen_i] ==1))
+    
+    if(n.gen_i != 0){
+      wspolne[i, j] <- n.gen_j / n.gen_i
+      wspolne[j, i] <- n.gen_j / n.gen_i
     }
-    else
-    {
-      wspolne[i, j]=wspolne[j, i]=0
-    }
+    
+
   }
+  print(paste(i, '/', nrow(geny)))
   
 }
 
 write.table(file=paste('Zbiory/wspolne_', nowotwor, '.txt', sep=""), wspolne)
+
+#wynik <- read.table(paste('Zbiory/wspolne_', nowotwor, '.txt', sep=""))
