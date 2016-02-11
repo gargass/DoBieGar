5d906a10e6c9fe17ffc2aca5825b3d2ee402c36d
Sebastian <sebastian.gargas92@gmail.com>
2016-02-11 21:25:20 +0100

    Co-occuring genes - n.gen_ij / n.gen_i

--- geny_wspolne_nowotwor.R
+++ geny_wspolne_nowotwor.R
@@ -16,15 +16,14 @@ for (i in 1:nrow(geny))
 {
   gen_i <- as.character(geny$x[i])
   n.gen_i <- sum(as.numeric(dane[,gen_i] == 1))
-  for (j in i:nrow(geny))
+  for (j in 1:nrow(geny))
   {
     gen_j <- as.character(geny$x[j])
     
-    n.gen_j <- sum(as.numeric(dane[,gen_j] == 1 & dane[, gen_i] ==1))
+    n.gen_ij <- sum(as.numeric(dane[,gen_i] == 1 & dane[, gen_j] ==1))
     
     if(n.gen_i != 0){
-      wspolne[i, j] <- n.gen_j / n.gen_i
-      wspolne[j, i] <- n.gen_j / n.gen_i
+      wspolne[j, i] <- n.gen_ij / n.gen_i
     }
     
 
